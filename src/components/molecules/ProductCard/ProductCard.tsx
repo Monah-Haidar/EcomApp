87ad3af9ef7b952271d9a70e2d2f7c6d28543847
Monah-Haidar <mnhaidar01@gmail.com>
2025-05-09 02:30:02 +0300

    Implement Product Card

---
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -0,0 +1,26 @@
+import {Image, Text, View} from 'react-native';
+import {useTheme} from '../../../store/ThemeStore/ThemeStore';
+import {productListStyles} from '../../../screens/Product/ProductListScreen/styles';
+
+const ProductCard = ({
+  source,
+  item,
+  itemWidth,
+}: {
+  source: any;
+  item: any;
+  itemWidth: number;
+}) => {
+  const {theme} = useTheme();
+  const styles = productListStyles(theme);
+  return (
+    <View style={[styles.card, {width: itemWidth}]}>
+      <Image source={source} style={styles.image} />
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.price}>${item.price}</Text>
+      <Text style={styles.description}>{item.description}</Text>
+    </View>
+  );
+};
+
+export default ProductCard;
