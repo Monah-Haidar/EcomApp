1b110f9203f083408ed2f4e408d29bfa3fb67094
Monah-Haidar <mnhaidar01@gmail.com>
2025-05-09 00:46:49 +0300

    Add Sign Up Screen

--- src/screens/Auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/Auth/SignUpScreen/SignUpScreen.tsx
@@ -1,10 +1,188 @@
-import {Text, View} from 'react-native';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useNavigation} from '@react-navigation/native';
+import {Controller, useForm} from 'react-hook-form';
+import {
+  Button,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import {z} from 'zod';
+import {useTheme} from '../../../store/ThemeStore/ThemeStore';
+import {formStyles} from '../../../styles/formStyles';
+
+const SignUpSchema = z.object({
+  name: z
+    .string({
+      required_error: 'Name is Required',
+      invalid_type_error: 'Name must be a string',
+    })
+    .min(3, {message: 'Name must be at least 3 characters long'})
+    .max(20, {message: 'Name must be at most 20 characters long'}),
+
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email({
+      message: 'Invalid email address',
+    }),
+
+  password: z
+    .string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Password must be a string',
+    })
+    .min(8, {message: 'Password must be at least 8 characters long'})
+    .max(50, {message: 'Password must be at most 50 characters long'}),
+
+  phoneNb: z
+    .string()
+    .regex(/^\d{10,15}$/, 'Phone number must be 10 to 15 digits long'),
+});
+
+type FormData = z.infer<typeof SignUpSchema>;
 
 const SignUpScreen = () => {
+  const navigation = useNavigation();
+  const {theme} = useTheme();
+  const styles = formStyles(theme);
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormData>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: 'john doe',
+      email: 'john@doe.com',
+      password: '12345678',
+      phoneNb: '0071143125',
+    },
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log('Form Data:', data);
+    navigation.navigate('Verification');
+  };
+
   return (
-    <View>
-      <Text>SignUpScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0} // adjust if needed
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        {/* <ScrollView contentContainerStyle={styles.container}> */}
+          <View style={styles.container}>
+          <Text style={styles.title}>Sign Up</Text>
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Name</Text>
+            <Controller
+              control={control}
+              name="name"
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={styles.input}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  placeholder="Enter your name"
+                />
+              )}
+            />
+            {errors.name && (
+              <Text style={styles.errorText}>{errors.name.message}</Text>
+            )}
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Email</Text>
+            <Controller
+              control={control}
+              name="email"
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={styles.input}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  placeholder="Enter your email"
+                  keyboardType="email-address"
+                />
+              )}
+            />
+            {errors.email && (
+              <Text style={styles.errorText}>{errors.email.message}</Text>
+            )}
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Password</Text>
+            <Controller
+              control={control}
+              name="password"
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={styles.input}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  placeholder="Enter your password"
+                  secureTextEntry
+                />
+              )}
+            />
+            {errors.password && (
+              <Text style={styles.errorText}>{errors.password.message}</Text>
+            )}
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Phone Number</Text>
+            <Controller
+              control={control}
+              name="phoneNb"
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={styles.input}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  placeholder="Enter your phone number"
+                  keyboardType="phone-pad"
+                />
+              )}
+            />
+            {errors.phoneNb && (
+              <Text style={styles.errorText}>{errors.phoneNb.message}</Text>
+            )}
+          </View>
+
+          <Button title="Sign Up" onPress={handleSubmit(onSubmit)} />
+
+          <Text style={styles.signUpText}>
+            Already have an account?{' '}
+            <Text
+              style={styles.signUpLink}
+              onPress={() => navigation.navigate('Login')}>
+              Login
+            </Text>
+          </Text>
+
+          
+          </View>
+        {/* </ScrollView> */}
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
---
+++ src/styles/formStyles.ts
@@ -0,0 +1,88 @@
+import {StyleSheet} from 'react-native';
+
+export const formStyles = (theme: any) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+    //   justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: theme.background,
+    },
+    body: {
+      flex: 1,
+    },
+    title: {
+      alignSelf: 'center',
+      fontSize: 24,
+      fontWeight: '700',
+      marginBottom: 40,
+    //   marginTop: 120,
+    },
+    label: {
+      marginBottom: 6,
+      fontWeight: '500',
+      color: '#333',
+    },
+    inputContainer: {
+      marginBottom: 20,
+      width: '80%',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#c4c4c4',
+      borderRadius: 6,
+      padding: 12,
+      fontSize: 16,
+      backgroundColor: '#f9f9f9',
+      width: '100%',
+    },
+    generalError: {
+      backgroundColor: '#ffebee',
+      color: '#d32f2f',
+      padding: 12,
+      borderRadius: 6,
+      marginBottom: 20,
+      width: '80%',
+      textAlign: 'center',
+    },
+    errorText: {
+      color: '#e53935',
+      marginTop: 6,
+      fontSize: 13,
+    },
+    signUpText: {
+      textAlign: 'center',
+      fontSize: 14,
+      color: '#333',
+      marginTop: 20,
+    },
+    signUpLink: {
+      color: '#0047FF',
+      fontWeight: '500',
+    },
+    loginButton: {
+      backgroundColor: '#0047FF',
+      paddingVertical: 14,
+      borderRadius: 30,
+      alignItems: 'center',
+      marginBottom: 30,
+    },
+    loginButtonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: '600',
+    },
+    backButton: {
+      position: 'absolute',
+      top: 20,
+      left: 20,
+      borderColor: '#c4c4c4',
+      borderWidth: 1,
+      borderRadius: 6,
+      padding: 6,
+    },
+    backButtonText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  });
