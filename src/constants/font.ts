dbfff0141a1b4a5759a38597f2e4db87deddcd62
Monah-Haidar <mnhaidar01@gmail.com>
2025-05-10 16:29:46 +0300

    Implement Custom Fonts

---
+++ src/constants/font.ts
@@ -0,0 +1,18 @@
+const FONT_FAMILY = {
+  POPPINS_REGULAR: 'Poppins-Regular',
+  POPPINS_MEDIUM: 'Poppins-Medium',
+  POPPINS_SEMI_BOLD: 'Poppins-SemiBold',
+  POPPINS_BOLD: 'Poppins-Bold',
+};
+
+const FONT_SIZE = {
+  XS: 12,
+  SM: 14,
+  MD: 16,
+  LG: 18,
+  XL: 22,
+  XXL: 28,
+  XXXL: 32,
+};
+
+export {FONT_FAMILY, FONT_SIZE};
