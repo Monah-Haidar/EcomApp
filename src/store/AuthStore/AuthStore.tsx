ea2f33d1190bfb669122790f46c2677d44761bc6
Monah-Haidar <mnhaidar01@gmail.com>
2025-05-10 22:10:23 +0300

    Refactor: AuthStore catch error

--- src/store/AuthStore/AuthStore.tsx
+++ src/store/AuthStore/AuthStore.tsx
@@ -28,6 +28,11 @@ export const AuthProvider = ({children}: {children: ReactNode}) => {
 };
 
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
 
-
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+};
