5d5511da95dea7c771a6c8cf027b36f4e79ec906
Monah-Haidar <mnhaidar01@gmail.com>
2025-05-18 16:05:18 +0300

    Add Profile Stack

---
+++ src/navigation/stacks/ProfileStack/ProfileStack.tsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProfileScreen } from "../../../screens/Profile/ProfileScreen";
+import { EditProfileScreen } from "../../../screens/Profile/EditProfileScreen";
+
+const Stack = createNativeStackNavigator();
+
+
+const ProfileStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default ProfileStack;
